665829a50ae0547e71b57ed240f8f037c9c68a64
Danial <danial.azimi.a@gmail.com>
2015-12-11 22:21:09 +0100

    Added score.cs, traphit.cs edited new layers and layer collision matrix

---
+++ SantaJam/Assets/Scripts/TrapHit.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapHit : MonoBehaviour {
+
+	public int points;
+
+	void Start () {
+		print("start");
+	}
+	
+
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter (Collision collision) {
+			print("wehweh");
+	}
+
+	void SubtractPoints() {
+
+	}
+}
