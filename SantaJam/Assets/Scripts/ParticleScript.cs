9baa7779866d30de87899dad6c9b86d55b89df20
Danial <danial.azimi.a@gmail.com>
2015-12-12 22:29:35 +0100

    Particle script

---
+++ SantaJam/Assets/Scripts/ParticleScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		ParticleDestroy();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void ParticleDestroy(){
+		Destroy(gameObject, GetComponent<ParticleSystem>().duration);
+	}
+
+}
